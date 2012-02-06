7d0bfa3062295c0dae7d8f7f86f03b2d557e97eb
James Finley <jamesfinley@gmail.com>
2012-02-05 20:07:44 -0600

    Added ResponsiveImage Component

---
+++ ResponsiveImage/responsiveimage.js
@@ -0,0 +1,27 @@
+jQuery.fn.responsiveImage = function (o) {
+	var $ = jQuery;
+	
+	var settings = {
+		'steps': new Array(
+			['desktop', 960],
+			['tablet', 720],
+			['small-tablet', 600],
+			['mobile-retina', 320, 2]
+		)
+	};
+	$.extend(settings, o);
+	
+	var pixelRatio = window.devicePixelRatio ? window.devicePixelRatio : 1;
+	var width = $(window).width();
+	
+	this.each(function () {
+		var el = $(this);
+		
+		for (var i=0, len=settings.steps.length; i<len; i++) {
+			if (settings.steps[i][1] <= width && el.attr('data-'+settings.steps[i][0]) && (settings.steps[i][2] !== undefined ? pixelRatio >= settings.steps[i][2] : false)) {
+				el.attr('src', el.attr('data-'+settings.steps[i][0]));
+				break;
+			}
+		}
+	});
+};
